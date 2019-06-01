d09584f7d2e859855755ea187ca412797d4fb1e2
CJ <cj@Christophers-MacBook-Pro.local>
2019-06-01 15:57:34 -0700

    added locality split and mode options. misc refactoring

--- scripts/anopheles_plots.R
+++ scripts/anopheles_plots.R
@@ -1,36 +1,96 @@
 #anopheles locator plots
-library(ggplot2);library(magrittr);library(data.table)
+library(ggplot2);library(magrittr);library(data.table);library(plyr);library(sp);library(cowplot)
 setwd("~/locator/")
 
-pred <- data.frame(fread("stats/anopheles_predlocs.txt"))
-test <- data.frame(fread("stats/anopheles_testlocs.txt"))
-colnames(pred) <- c("pred_long","pred_lat")
-colnames(test) <- c("true_long","true_lat")
-pd <- cbind(pred,test)
-pred$type <- "prediction"
-test$type <- "truth"
-colnames(pred) <- c("long","lat","type")
-colnames(test) <- c("long","lat","type")
-longpd <- rbind(pred,test)
+pred <- data.frame(fread("out/anopheles_predlocs.txt"))[-1,-1]
+colnames(pred) <- c("longitude","latitude","sampleID")
+samples <- fread("data/anopheles_samples_sp.txt")
+pd <- merge(pred,samples,by="sampleID",all=T)
+pd$species[is.na(pd$species)] <- "unknown"
+trainlocs <- ddply(pd,.(longitude.y,latitude.y,species),summarize,n=length(species))
 
-refsamples <- fread("data/anopheles_samples_sp.txt")
-refsamples <- subset(refsamples,refsamples$species=="gambiae")
+#mean error
+plocs=as.matrix(pd[!is.na(pd$longitude.x),c("longitude.x","latitude.x")])
+tlocs=as.matrix(pd[!is.na(pd$longitude.x),c("longitude.y","latitude.y")])
+dists=sapply(1:nrow(plocs),function(e) spDistsN1(t(as.matrix(plocs[e,])),
+                                           t(as.matrix(tlocs[e,])),longlat = T))
+mean(dists)
 
 map <- map_data("world")
 p <- ggplot()+coord_map()+
   theme(axis.title.x = element_blank(),
         axis.title.y=element_blank(),
-        legend.position=c(0.2,0.25))+
-  scale_color_manual(values=c("steelblue3","orangered"),name="")+
-  xlim(min(longpd$long)-3,max(longpd$long)+3)+
-  ylim(min(longpd$lat)-3,max(longpd$lat)+3)+
-  geom_path(data=map,aes(x=long,y=lat,group=group),col="grey30",lwd=0.1)+
-  geom_segment(data=pd,aes(x=true_long,xend=pred_long,y=true_lat,yend=pred_lat),lwd=0.25)+
-  #geom_point(data=refsamples,aes(x=longitude,y=latitude),shape=1,size=1,col='black')+
-  geom_point(data=longpd,aes(x=long,y=lat,col=type),shape=1,size=1)
-p <- p+guides(color=guide_legend(keyheight=0.12,keywidth=0.1,default.unit = "inch"))
-pdf("fig/gambiae_map.pdf",width=3.5,height=2,useDingbats = F)
+        axis.text=element_text(size=7),
+        legend.position=c(0,0.25),
+        legend.box = "horizontal",
+        legend.title=element_text(size=7),
+        legend.text=element_text(size=6),
+        legend.spacing = unit(0,"mm"))+
+  scale_color_manual(values=c("steelblue3","orangered","gold2"),name="")+
+  scale_fill_manual(values=c("steelblue3","orangered","gold2"),name="species")+
+  scale_size_continuous(name="samples",breaks=c(2,10,20),labels=c(10,50,100))+
+  xlim(min(pd$longitude.y)-4,max(pd$longitude.y)+4)+
+  ylim(min(pd$latitude.y)-4,max(pd$latitude.y)+4)+
+  geom_path(data=map,aes(x=long,y=lat,group=group),col="grey50",lwd=0.2)+
+  geom_segment(data=pd,aes(x=longitude.x,xend=longitude.y,y=latitude.x,yend=latitude.y),lwd=0.2)+
+  geom_point(data=pd,aes(x=longitude.x,y=latitude.x,fill=species),size=1,shape=21,stroke=0.02)+
+  geom_point(data=trainlocs,aes(x=longitude.y,y=latitude.y,fill=species,size=n/5),shape=21,stroke=0.1,alpha=0.3)
+p <- p+guides(fill=guide_legend(keyheight=0.12,
+                                 keywidth=0.1,
+                                 default.unit = "inch",
+                                 override.aes=list(size=3)),
+              size=guide_legend(keyheight=0.12,
+                                 keywidth=0.1,
+                                 default.unit = "inch"))
+
+pdf("fig/gambiae_map.pdf",width=3.25,height=2,useDingbats = F)
 print(p)
 dev.off()
 
+p <- ggplot()+coord_map()+
+  theme(axis.title.x = element_blank(),
+        axis.title.y=element_blank(),
+        axis.text=element_text(size=7),
+        legend.position="bottom",
+        legend.box = "vertical",
+        legend.title=element_text(size=7),
+        legend.text=element_text(size=6,face="italic"),
+        legend.spacing = unit(0,"mm"),
+        legend.box.margin = margin(-20,0,0,25),
+        legend.spacing.y=unit(-.2,"inch"))+
+  scale_color_manual(values=c("steelblue3","orangered","gold2"),name="",guide=F)+
+  scale_fill_manual(values=c("steelblue3","orangered","gold2"),name="Species:")+
+  scale_size_continuous(name="Samples:",breaks=c(2,10,20),labels=c(10,50,100))+
+  xlim(min(pd$longitude.y)-4,max(pd$longitude.y)+4)+
+  ylim(min(pd$latitude.y)-4,max(pd$latitude.y)+4)+
+  geom_path(data=map,aes(x=long,y=lat,group=group),col="grey50",lwd=0.2)+
+  geom_segment(data=pd,aes(x=longitude.x,xend=longitude.y,y=latitude.x,yend=latitude.y),lwd=0.2)+
+  geom_point(data=pd,aes(x=longitude.x,y=latitude.x,fill=species),size=1,shape=21,stroke=0.01)+
+  geom_point(data=trainlocs,aes(x=longitude.y,y=latitude.y,col=species,size=n/5),shape=1,stroke=0.3)
+p <- p+guides(fill=guide_legend(keyheight=0.12,
+                                keywidth=0.1,
+                                default.unit = "inch",
+                                override.aes=list(size=3)),
+              size=guide_legend(keyheight=0.12,
+                                keywidth=0.1,
+                                default.unit = "inch"))
 
+p2 <- ggplot(data=data.frame(dists),aes(x=dists))+
+  theme(axis.title=element_text(size=7,vjust=3),
+        axis.text=element_text(size=6),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.title.y=element_blank())+
+  #scale_x_continuous(breaks=c(0,750,1500))+
+  geom_density(lwd=0.3,fill="grey")+
+  xlab("Error (km)")+
+  geom_vline(aes(xintercept=median(dists)),lwd=0.35,col="darkgreen")
+
+
+pdf("fig/gambiae_combo_map.pdf",width=3.5,height=2.5,useDingbats = F)
+ggdraw()+
+  draw_plot(p,0,0,1,1)+
+  draw_plot(p2,0.1,.25,.3,.45)
+dev.off()
+
+print(median(dists))
--- scripts/locator.py
+++ scripts/locator.py
@@ -4,7 +4,7 @@ import numpy as np, pandas as pd, tensorflow as tf
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 import argparse
-#os.environ["CUDA_VISIBLE_DEVICES"]="0" #set to "" to run on CPU
+os.environ["CUDA_VISIBLE_DEVICES"]="1" #set to "" to run on CPU
 #config = tensorflow.ConfigProto(device_count={'CPU': 60})
 #sess = tensorflow.Session(config=config)
 # config = tf.ConfigProto()
@@ -14,23 +14,34 @@ import argparse
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--vcf",help="VCF with SNPs for all samples.")
-parser.add_argument("--sample_data",help="tab-delimited text file with columns\
-                                         'sampleID \t longitude \t latitude'.\
-                                         SampleIDs must exactly match those in the \
-                                         training VCF. Longitude and latitude for \
-                                         samples without known geographic origin should \
-                                         be NA. By default, locations will be predicted \
-                                         for all samples without locations. If the \
-                                         train_split parameter is provided, locations \
-                                         will be predicted for randomly selected \
-                                         individuals.")
-parser.add_argument("--train_split",default=None,type=float)
+parser.add_argument("--sample_data",
+                    help="tab-delimited text file with columns\
+                         'sampleID \t longitude \t latitude'.\
+                          SampleIDs must exactly match those in the \
+                          training VCF. Longitude and latitude for \
+                          samples without known geographic origin should \
+                          be NA. By default, locations will be predicted \
+                          for all samples without locations. If the \
+                          train_split parameter is provided, locations \
+                          will be predicted for randomly selected \
+                          individuals.")
+parser.add_argument("--mode",default="cv",
+                    help="'cv' splits the sample by train_split \
+                        and predicts on the test set. \
+                        'predict' extracts samples with non-NaN \
+                        coordinates, splits those by train_split \
+                        for training and evaluation, and returns \
+                        predictions for samples with NaN coordinates.")
+parser.add_argument("--locality_split",default=False,type=bool,
+                    help="Split training and testing evenly by locality.")
+parser.add_argument("--train_split",default=None,type=float,
+                    help="0-1, proportion of samples to use for training.")
 parser.add_argument("--batch_size",default=128,type=int)
 parser.add_argument("--max_epochs",default=5000,type=int)
 parser.add_argument("--max_SNPs",default=None,type=int)
 parser.add_argument("--min_mac",default=2,type=int)
 parser.add_argument("--patience",type=int,default=100)
-parser.add_argument("--model",default="CNN")
+parser.add_argument("--model",default="dense")
 parser.add_argument("--outname")
 parser.add_argument("--outdir")
 args=parser.parse_args()
@@ -38,18 +49,54 @@ args=parser.parse_args()
 #debugging params
 # args=argparse.Namespace(vcf="/Users/cj/locator/data/ag1000g2L_1e6_to_2.5e6.vcf.gz",
 #                         sample_data="/Users/cj/locator/data/anopheles_samples_sp.txt",
-#                         train_split=0.9,
+#                         train_split=0.8,
 #                         batch_size=128,
 #                         max_epochs=5000,
 #                         patience=200,
 #                         max_SNPs=None,
 #                         min_mac=2,
-#                         outname="anopheles",
+#                         outname="pabu",
 #                         model="dense",
-#                         outdir="/Users/cj/locator/stats/")
+#                         outdir="/Users/cj/locator/out/",
+#                         mode="cv",
+#                         locality_split=False)
+
+#helper functions
+def split_by_locality():
+    ntest=len(train)-round(args.train_split*len(train))
+    l2=np.unique(locs[:,0])
+    l2=l2[~np.isnan(l2)]
+    l2=l2[np.random.choice(range(len(l2)),len(l2),replace=False)]
+    pop_indices=[]
+    for i in l2:
+        popinds=np.argwhere(locs[:,0]==i)
+        popinds=[x[0] for x in popinds]
+        pop_indices.append(popinds)
+    test=[]
+    while len(test)<ntest:
+        pop_indices=np.array(pop_indices)[np.random.choice(range(len(pop_indices)),len(pop_indices))]
+        for i in pop_indices:
+            if len(test)<ntest:
+                k=np.random.choice(i)
+                if not k in test:
+                    test.append(k)
+    test=np.array(test)
+    if args.mode=="predict":
+        train=np.array([x for x in train if x not in test])
+    elif args.mode=="cv":
+        train=np.array([x for x in range(len(locs)) if x not in test])
+    return test,train
+
+def replace_md(ac,af):
+    for i in tqdm(range(np.shape(ac)[0])): #replace missing sites with binomial(2,mean_allele_frequency)
+        for j in range(np.shape(ac)[1]):
+            if(missingness[i,j]):
+                ac[i,j]=np.random.binomial(2,af[i])
+    return ac
 
 #load genotype matrices from VCF
-vcf=allel.read_vcf(args.vcf)
+print("reading VCF")
+vcf=allel.read_vcf(args.vcf,log=sys.stderr)
 genotypes=allel.GenotypeArray(vcf['calldata/GT'])
 samples=vcf['samples']
 
@@ -62,15 +109,21 @@ if not all([sample_data['sampleID2'][x]==samples[x] for x in range(len(samples))
     print("sample ordering failed! Check that sample IDs match the VCF.")
 print("loaded "+str(np.shape(genotypes))+" genotypes\n\n")
 
-#drop low-frequency sites
+#SNP filters
+print("replacing missing data with binomial(2,allele_frequency)")
+missingness=genotypes.is_missing()
 derived_counts=genotypes.count_alleles()[:,1]
-af_filter=[x >= args.min_mac for x in derived_counts] #drop low-frequency alleles
-genotypes=genotypes[af_filter,:,:]
-
+ninds=np.array([np.sum(x) for x in ~missingness])
+af=np.array([derived_counts[x]/(2*ninds[x]) for x in range(len(ninds))])
+ac_filter=[x >= args.min_mac for x in derived_counts] #drop SNPs with minor allele < min_mac
+genotypes=genotypes[ac_filter,:,:]
 if not args.max_SNPs==None:
-    ac=genotypes.to_allele_counts()[0:args.max_SNPs,:,1]
+    snps_to_run=np.random.choice(range(len(ac)),args.max_SNPs,replace=False)
+    ac=genotypes.to_allele_counts()[snps_to_run,:,1]
+    ac=replace_md(ac,af)
 else:
     ac=genotypes.to_allele_counts()[:,:,1]
+    ac=replace_md(ac,af)
 locs=np.array(sample_data[["longitude","latitude"]])
 print("running on "+str(len(ac))+" genotypes after filtering\n\n\n")
 
@@ -82,26 +135,35 @@ sdlat=np.nanstd(locs[:,1])
 locs=np.array([[(x[0]-meanlong)/sdlong,(x[1]-meanlat)/sdlat] for x in locs])
 
 #split training, testing, and prediction sets
-if any(np.isnan(locs[:,0])):
+if args.mode=="predict": #prediction mode
     train=np.argwhere(~np.isnan(locs[:,0]))
     train=[x[0] for x in train]
-    pred=[x for x in range(len(locs)) if not x in train]
-    test=np.random.choice(train,len(train)-round(args.train_split*len(train)))
-    train=[x for x in train if x not in test]
+    pred=np.array([x for x in range(len(locs)) if not x in train])
+    if(args.locality_split):
+        test,train=split_by_locality()
+    else:
+        test=np.array(train[np.random.choice(train,1-round(args.train_split*len(locs)))])
+        train=np.array([x for x in train if x not in test])
     traingen=np.transpose(ac[:,train])
     trainlocs=locs[train]
     testgen=np.transpose(ac[:,test])
     testlocs=locs[test]
     predgen=np.transpose(ac[:,pred])
-else:
-    train=np.random.choice(range(len(locs)),
-                           round(args.train_split*len(locs)),
-                           replace=False)
-    test=[x for x in range(len(locs)) if not x in train]
+elif args.mode=="cv": #cross-validation mode
+    if args.locality_split:
+        test,train=split_by_locality()
+        pred=test
+    else:
+        train=np.random.choice(range(len(locs)),
+                               round(args.train_split*len(locs)),
+                               replace=False)
+        test=np.array([x for x in range(len(locs)) if not x in train])
+        pred=test
     traingen=np.transpose(ac[:,train])
     trainlocs=locs[train]
     testgen=np.transpose(ac[:,test])
     testlocs=locs[test]
+    predgen=testgen
 
 #define a 1D CNN for regression
 from keras.models import Sequential
@@ -115,14 +177,14 @@ if args.model=="CNN":
     model.add(layers.Conv1D(64, 7, activation='relu',input_shape=(np.shape(train_x)[1],1)))
     model.add(layers.Conv1D(32, 7, activation='relu'))
     model.add(layers.Dense(2))
-    odel.compile(optimizer="Adam",
+    model.compile(optimizer="Adam",
                   loss=keras.losses.mean_squared_error,
                   metrics=['mae'])
 
 if args.model=="dense":
     train_x=traingen
     test_x=testgen
-    pred_x=testgen
+    pred_x=predgen
     model = Sequential()
     model.add(layers.Dense(256, activation='elu',input_shape=(np.shape(train_x)[1],)))
     model.add(layers.Dense(128,activation='elu'))
@@ -137,7 +199,7 @@ if args.model=="dense":
 
 #fit model and choose best weights
 checkpointer=keras.callbacks.ModelCheckpoint(
-                                filepath='weights.hdf5',
+                                filepath=os.path.join(args.outdir,"weights.hdf5"),
                                 verbose=1,
                                 save_best_only=True,
                                 monitor="val_loss",
@@ -150,38 +212,44 @@ history = model.fit(train_x, trainlocs,
                     batch_size=args.batch_size,
                     validation_data=(test_x,testlocs),
                     callbacks=[checkpointer,earlystop])
-model.load_weights("weights.hdf5")
+model.load_weights(os.path.join(args.outdir,"weights.hdf5"))
 
 #predict and plot
-pred=model.predict(pred_x)
-pred=np.array([[x[0]*sdlong+meanlong,x[1]*sdlat+meanlat] for x in pred]) #reverse normalization
-testlocs=np.array([[x[0]*sdlong+meanlong,x[1]*sdlat+meanlat] for x in testlocs])
-np.savetxt(fname=os.path.join(args.outdir,args.outname+"_predlocs.txt"),X=pred)
-np.savetxt(fname=os.path.join(args.outdir,args.outname+"_testlocs.txt"),X=testlocs)
+prediction=model.predict(pred_x)
+predout=pd.DataFrame(np.array([[x[0]*sdlong+meanlong,x[1]*sdlat+meanlat] for x in prediction])) #reverse normalization
+predout['sampleID']=samples[pred]
+predout.to_csv(os.path.join(args.outdir,args.outname+"_predlocs.txt"))
 
 #print correlation coefficient for longitude
-r2_long=np.corrcoef(pred[:,0],testlocs[:,0])[0][1]**2
-r2_lat=np.corrcoef(pred[:,1],testlocs[:,1])[0][1]**2
-print("R2(longitude)="+str(r2_long)+"\nR2(latitude)="+str(r2_lat))
+if args.mode=="cv":
+    r2_long=np.corrcoef(prediction[:,0],testlocs[:,0])[0][1]**2
+    r2_lat=np.corrcoef(prediction[:,1],testlocs[:,1])[0][1]**2
+    print("R2(longitude)="+str(r2_long)+"\nR2(latitude)="+str(r2_lat))
+elif args.mode=="predict":
+    p2=model.predict(test_x)
+    r2_long=np.corrcoef(p2[:,0],testlocs[:,0])[0][1]**2
+    r2_lat=np.corrcoef(p2[:,1],testlocs[:,1])[0][1]**2
+    print("R2(longitude)="+str(r2_long)+"\nR2(latitude)="+str(r2_lat))
 
-fig = plt.figure(figsize=(4,2),dpi=200)
-plt.rcParams.update({'font.size': 7})
-ax1=fig.add_axes([0,.59,0.25,.375])
-ax1.plot(history.history['val_loss'][5:],"-",color="black",lw=0.5)
-ax1.set_xlabel("Validation Loss")
-ax1.set_yscale("log")
+if args.mode=="cv":
+    fig = plt.figure(figsize=(4,2),dpi=200)
+    plt.rcParams.update({'font.size': 7})
+    ax1=fig.add_axes([0,.59,0.25,.375])
+    ax1.plot(history.history['val_loss'][5:],"-",color="black",lw=0.5)
+    ax1.set_xlabel("Validation Loss")
+    ax1.set_yscale("log")
 
-ax2=fig.add_axes([0,0,0.25,.375])
-ax2.plot(history.history['loss'][5:],"-",color="black",lw=0.5)
-ax2.set_xlabel("Training Loss")
-ax2.set_yscale("log")
+    ax2=fig.add_axes([0,0,0.25,.375])
+    ax2.plot(history.history['loss'][5:],"-",color="black",lw=0.5)
+    ax2.set_xlabel("Training Loss")
+    ax2.set_yscale("log")
 
-ax3=fig.add_axes([0.44,0.01,0.55,.94])
-ax3.scatter(testlocs[:,0],testlocs[:,1],s=4,linewidth=.4,facecolors="none",edgecolors="black")
-ax3.scatter(pred[:,0],pred[:,1],s=2,color="black")
-for x in range(len(pred)):
-    ax3.plot([pred[x,0],testlocs[x,0]],[pred[x,1],testlocs[x,1]],lw=.3,color="black")
-#ax3.set_xlabel("simulated X coordinate")
-#ax3.set_ylabel("predicted X coordinate")
-#ax3.set_title(r"$R^2$="+str(round(cor[0][1]**2,4)))
-fig.savefig(os.path.join(args.outdir,args.outname+"_fitplot.pdf"),bbox_inches='tight')
+    ax3=fig.add_axes([0.44,0.01,0.55,.94])
+    ax3.scatter(testlocs[:,0],testlocs[:,1],s=4,linewidth=.4,facecolors="none",edgecolors="black")
+    ax3.scatter(prediction[:,0],prediction[:,1],s=2,color="black")
+    for x in range(len(pred)):
+        ax3.plot([prediction[x,0],testlocs[x,0]],[prediction[x,1],testlocs[x,1]],lw=.3,color="black")
+    #ax3.set_xlabel("simulated X coordinate")
+    #ax3.set_ylabel("predicted X coordinate")
+    #ax3.set_title(r"$R^2$="+str(round(cor[0][1]**2,4)))
+    fig.savefig(os.path.join(args.outdir,args.outname+"_fitplot.pdf"),bbox_inches='tight')
