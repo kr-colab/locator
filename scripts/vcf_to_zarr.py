3adc90eff7406a5a4002cd67f614717a067316d7
Clara Rehmann <crehmann@uoregon.edu>
2023-04-18 17:09:42 +0000

    simplifying zarr output

--- scripts/vcf_to_zarr.py
+++ scripts/vcf_to_zarr.py
@@ -4,6 +4,4 @@ parser.add_argument("--vcf",help="path to VCF (or .vcf.gz)")
 parser.add_argument("--zarr",help="path for zarr output")
 args=parser.parse_args()
 
-allel.vcf_to_zarr(args.vcf,args.zarr)
-
-
+allel.vcf_to_zarr(args.vcf,args.zarr,fields=['variants/POS','calldata/GT','samples'])
