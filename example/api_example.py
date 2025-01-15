2418b696209779d30667818b57848a4a34e80f65
andrewkern <adkern@uoregon.edu>
2025-01-15 10:01:56 -0800

    api example cont

--- example/api_example.py
+++ example/api_example.py
@@ -1,34 +1,47 @@
 # Example API for locator
 
-from locator import Locator
+from locator import Locator, plot_predictions
 
 
 # Override some defaults
 locator = Locator({"out": "my_analysis", "train_split": 0.9, "batch_size": 64})
 
 # For your specific example, you might want:
-locator = Locator({"out": "test_output", "sample_data": "data/test_sample_data.txt"})
+locator = Locator(
+    {
+        "out": "windows",
+        "sample_data": "data/test_sample_data.txt",
+        "zarr": "data/test_genotypes.zarr",
+    }
+)
 
 # Load and sort data
 genotypes, samples = locator.load_genotypes(
-    vcf="data/test_genotypes.vcf.gz",
-)
-
-locator.sort_samples(
-    samples=samples,
-    sample_data_file="data/test_sample_data.txt",
+    # vcf="data/test_genotypes.vcf.gz",
+    zarr="data/test_genotypes.zarr",
 )
 
 # Train the model
-locator.train(
-    genotypes=genotypes,
-    samples=samples,
-)
+# locator.train(genotypes=genotypes, samples=samples)
 
 # do prediction
-locator.predict(verbose=True)
+# all_predictions = locator.predict(verbose=True, return_df=True)
 
 # do jacknife
-locator.run_jacknife(genotypes, samples)
+all_predictions = locator.run_jacknife(genotypes, samples, return_df=True)
 
-# plot the results
+# do run_windows
+# all_predictions = locator.run_windows(genotypes, samples, return_df=True)
+
+
+## run bootstrap
+# all_predictions = locator.run_bootstraps(
+#     genotypes, samples, n_bootstraps=3, return_df=True
+# )
+
+plot_predictions(
+    predictions=all_predictions, sample_data=samples, out_prefix="jacknife_example"
+)
+
+
+print(all_predictions.head())
